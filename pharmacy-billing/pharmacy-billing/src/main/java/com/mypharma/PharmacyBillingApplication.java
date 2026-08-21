20fa29fb8e83521a7a3f041fb532dfc2b2ea7637
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-21 23:31:18 +0530

    Create PharmacyBillingApplication.java

---
+++ pharmacy-billing/pharmacy-billing/src/main/java/com/mypharma/PharmacyBillingApplication.java
@@ -0,0 +1,13 @@
+package com.mypharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyBillingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyBillingApplication.class, args);
+	}
+
+}
