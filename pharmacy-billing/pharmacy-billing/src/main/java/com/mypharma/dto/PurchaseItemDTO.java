6fe68e7999aacea2ef3348b6379e40736a32ca63
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-21 23:30:01 +0530

    Create PurchaseItemDTO.java

---
+++ pharmacy-billing/pharmacy-billing/src/main/java/com/mypharma/dto/PurchaseItemDTO.java
@@ -0,0 +1,27 @@
+package com.mypharma.dto;
+
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PurchaseItemDTO {
+
+    private Long medicineId;
+
+    private String batchNumber;
+
+    private LocalDate expiryDate;
+
+    private Integer quantity;
+
+    private BigDecimal purchasePrice;
+
+    private BigDecimal sellingPrice;
+}
