0b55700f3ccfaa71fa1420d7aaefd640054ec83c
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-21 23:30:47 +0530

    Create InsufficientStockException.java

---
+++ pharmacy-billing/pharmacy-billing/src/main/java/com/mypharma/exception/InsufficientStockException.java
@@ -0,0 +1,9 @@
+package com.mypharma.exception;
+
+
+public class InsufficientStockException extends RuntimeException {
+
+    public InsufficientStockException(String message) {
+        super(message);
+    }
+}
