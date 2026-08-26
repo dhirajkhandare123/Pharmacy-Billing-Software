f71b962efca345ac5f73f6cd6886a4543da21a43
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-26 17:08:06 +0530

    Update PurchaseItemRepository.java

--- pharmacy-billing/pharmacy-billing/src/main/java/com/mypharma/repository/PurchaseItemRepository.java
+++ pharmacy-billing/pharmacy-billing/src/main/java/com/mypharma/repository/PurchaseItemRepository.java
@@ -3,6 +3,7 @@ package com.mypharma.repository;
 import com.mypharma.entity.PurchaseItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface PurchaseItemRepository extends JpaRepository<PurchaseItem, Long> {
@@ -13,4 +14,15 @@ public interface PurchaseItemRepository extends JpaRepository<PurchaseItem, Long
             Long medicineId,
             String batchNumber
     );
+
+    // for Inventry
+
+    List<PurchaseItem> findByExpiryDateBefore(LocalDate date);
+
+    List<PurchaseItem> findByExpiryDateBetween(
+            LocalDate startDate,
+            LocalDate endDate
+    );
+
+
 }
