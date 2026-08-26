b818ac0ec300064242ab107d1f31c50d44a97b08
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-26 17:07:54 +0530

    Create SaleResponseDTO.java

---
+++ pharmacy-billing/pharmacy-billing/src/main/java/com/mypharma/dto/SaleResponseDTO.java
@@ -0,0 +1,27 @@
+package com.mypharma.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SaleResponseDTO {
+
+    private Long id;
+
+    private String invoiceNumber;
+
+    private LocalDateTime saleDate;
+
+    private BigDecimal totalAmount;
+
+    private String paymentStatus;
+
+    private List<SaleItemResponseDTO> items;
+}
