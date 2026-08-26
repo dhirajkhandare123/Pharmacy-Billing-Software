e49693bddd63bf93e565f5ddcf01886c902f2d6d
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-26 17:07:50 +0530

    Create SaleRequestDTO.java

---
+++ pharmacy-billing/pharmacy-billing/src/main/java/com/mypharma/dto/SaleRequestDTO.java
@@ -0,0 +1,22 @@
+package com.mypharma.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class SaleRequestDTO {
+
+    private String invoiceNumber;
+
+    private BigDecimal totalAmount;
+
+    private String paymentStatus;
+
+    private List<SaleItemRequestDTO> items;
+}
