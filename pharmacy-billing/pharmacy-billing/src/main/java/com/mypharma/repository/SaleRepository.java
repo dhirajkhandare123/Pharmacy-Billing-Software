0ddeb17451daa8c7b1ecfade5dfffed5d3fcd22d
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-09-01 23:48:54 +0530

    Update SaleRepository.java

--- pharmacy-billing/pharmacy-billing/src/main/java/com/mypharma/repository/SaleRepository.java
+++ pharmacy-billing/pharmacy-billing/src/main/java/com/mypharma/repository/SaleRepository.java
@@ -2,8 +2,20 @@ package com.mypharma.repository;
 
 import com.mypharma.entity.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
     boolean existsByInvoiceNumber(String invoiceNumber);
+
+    // for dashboard
+
+    @Query("""
+       SELECT COALESCE(SUM(s.totalAmount), 0)
+       FROM Sale s
+       WHERE s.saleDate = CURRENT_DATE
+       """)
+    Double getTodaysSales();
+
+
 }
