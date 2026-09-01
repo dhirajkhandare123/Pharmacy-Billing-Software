255d08a99abd5f71b6bca53bfbb6c7ce06c5c062
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-09-01 23:48:50 +0530

    Update MedicineRepository.java

--- pharmacy-billing/pharmacy-billing/src/main/java/com/mypharma/repository/MedicineRepository.java
+++ pharmacy-billing/pharmacy-billing/src/main/java/com/mypharma/repository/MedicineRepository.java
@@ -3,6 +3,7 @@ package com.mypharma.repository;
 
 import com.mypharma.entity.Medicine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,4 +12,21 @@ public interface MedicineRepository extends JpaRepository<Medicine, Long> {
     List<Medicine> findByStockQuantityLessThanEqual(Integer quantity);
 
     List<Medicine> findByNameContainingIgnoreCase(String name);
+
+    // for dashboard
+    @Query("""
+           SELECT COUNT(m)
+           FROM Medicine m
+           WHERE m.stockQuantity <= m.minimumStockLevel
+           """)
+    Long countLowStock();
+
+//    @Query("""
+//           SELECT COUNT(m)
+//           FROM Medicine m
+//           WHERE m.expiryDate IS NOT NULL
+//           AND m.expiryDate >= CURRENT_DATE
+//           AND m.expiryDate <= CURRENT_DATE + 30
+//           """)
+//    Long countExpiringSoon();
 }
